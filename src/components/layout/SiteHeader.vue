0de74ee790110c077ab0f93f652ef4142c055d59
Calvin Kamp <calvin.kamp6696@gmail.com>
2026-07-11 11:21:09 +0200

    feat: update header-component

--- src/components/layout/SiteHeader.vue
+++ src/components/layout/SiteHeader.vue
@@ -1,6 +1,6 @@
 <script setup lang="ts">
 import type { NavItem } from '.'
-import { BaseLink } from '../base'
+import { BaseButton, BaseLink } from '../base'
 import { ContactRound, FolderGit2, Home, UserRound, type LucideIcon } from '@lucide/vue'
 
 interface HeaderNavItem extends NavItem {
@@ -16,18 +16,18 @@ const headerNavigation: HeaderNavItem[] = [
 </script>
 <template>
   <header
-    class="fixed top-6 left-1/2 -translate-x-1/2 py-4 px-12 bg-surface border border-border rounded-full"
+    class="fixed top-6 left-1/2 -translate-x-1/2 py-2 px-6 bg-surface border border-border rounded-full z-10"
   >
     <nav>
       <ul class="flex gap-6 align-center">
         <li class="" v-for="navItem in headerNavigation" :key="navItem.href">
-          <BaseLink :href="navItem.href">
-            <component :is="navItem.icon" aria-hidden="true" />
+          <BaseButton :href="navItem.href" :as="BaseLink" variant="ghost" size="icon">
+            <component :is="navItem.icon" class="size-5" aria-hidden="true" />
 
             <span class="sr-only lg:not-sr-only">
               {{ navItem.label }}
             </span>
-          </BaseLink>
+          </BaseButton>
         </li>
       </ul>
     </nav>
